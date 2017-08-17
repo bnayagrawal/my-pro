dd607395a2784446e81d3b7d6e2dd6a50973f245
bnayagrawal <bnayagrawal@gmail.com>
2017-08-17 19:39:07 +0530

    Defined multiple components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'my-app',
@@ -11,14 +12,7 @@ import { Component } from '@angular/core';
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name">
-      </div>
-    </div>`,
+  <hero-detail [hero]="selectedHero"></hero-detail>`,
   styles: [`
     .selected {
       background-color: #CFD8DC !important;
@@ -77,11 +71,6 @@ export class AppComponent  {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
-
-export class Hero {
-  id: number;
-  name: string;
 }
 
 const HEROES: Hero[] = [
